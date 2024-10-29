c5bdd38af8960c39f8e9ba71101c4f3d8697875a
Nosia Akter <96822992+nosianuri@users.noreply.github.com>
2024-10-29 16:02:34 +0600

    update equal sum

--- Module 19/equal_sum.cpp
+++ Module 19/equal_sum.cpp
@@ -1,45 +1,45 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int dp[1005][1005];
-int subset_sum(int n, int a[], int s)
-{
-  if(n==0)
-  {
-    if(s==0) 
-    return 1;
-    else 
-    return 0;
-  }
-  if(dp[n][s] != -1) return dp[n][s];
-  if(a[n-1] <= s)
-  {
-    int op1 = subset_sum(n - 1, a,s-a[n-1]);
-    int op2 = subset_sum(n-1,a,s);
-    return dp[n][s] = op1 + op2;
-  }
-  else{
-    return dp[n][s] = subset_sum(n-1, a, s);
-  }
-}
+
+
 
 int main()
 {
     int n;
     cin>>n;
+    int s=0;
     int a[n];
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n;i++)
     {
       cin>>a[i];
+      s+a[i];
     }
-    int s;
-    cin>>s;
-    for(int j=0;j<=s;j++)
+    if(s%2==0)
     {
-      for(int j=0; j<=s; j++)
+      int sum = s/2;
+      int dp[n+1][sum+1];
+      bool dp[0][0] = true;
+      for(int i=1;i<=sum;i++) dp[0][i]=false;
+      for(int i=1;i<=n;i++)
       {
-        dp[i][j] = -1;
+        for(int j=0;j<=sum;j++)
+        {
+          if(a[i-1]<=j)
+          {
+            dp[i][j]=dp[i-1][j-a[i-1]] || dp[i-1][j];
+          }
+          else{
+            dp[i][j]=dp[i-1][j];
+          }
+        }
       }
+      if(dp[n][sum]) cout<<"YES"<<endl;
+      else cout<<"No"<<endl;
     }
-    cout<<subset_sum(n,a,s);
+    else
+    {
+      cout<<"No"<<endl;
+    }
     return 0;
 }
